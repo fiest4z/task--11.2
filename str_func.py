bbefbc6267e50b40dfebc611697210ee8798ba09
Mikhail Belyaev <fiestaz1337@gmail.com>
2024-05-19 19:16:36 +0300

    new func title + docstrs

--- str_func.py
+++ str_func.py
@@ -1,3 +1,14 @@
 def zagl():
+    '''
+    Все буквы большие
+    '''
     user_string = input().upper()
     return user_string
+
+def first_big_letter():
+    '''
+    Первая буква большая
+    '''
+    user_string = input().title()
+    return user_string
+print(first_big_letter())
